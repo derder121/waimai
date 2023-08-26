2e32a786e9664b2d9c13e6f9feda48dfed2fee7b
0Charles0 <1913896486@qq.com>
2023-08-26 17:24:28 +0800

    1

---
+++ src/main/java/com/kece/fanta/FantaApplication.java
@@ -0,0 +1,14 @@
+package com.kece.fanta;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class FantaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FantaApplication.class, args);
+        log.info("项目启动成功...");
+    }
+}
