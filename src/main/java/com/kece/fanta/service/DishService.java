fd30652bac2ce0f6e9036c75da8571d0b82336e1
derder121 <2575807941@qq.com>
2023-08-28 16:31:00 +0800

    菜品功能完善

--- src/main/java/com/kece/fanta/service/DishService.java
+++ src/main/java/com/kece/fanta/service/DishService.java
@@ -8,4 +8,10 @@ public interface DishService extends IService<Dish> {
 
     // 新增菜品，同时新增菜品对应的口味数据，需要操作两张表
     public void saveWithFlavor(DishDto dishDto);
+
+    //根据id查询菜品信息和口味信息，操作两张表
+    public DishDto getByIdWithFlavor(Long id);
+
+    //更新菜品表以及对应的口味
+    public void updateWithFlavor(DishDto dishDto);
 }
--- src/main/java/com/kece/fanta/service/impl/DishServiceImpl.java
+++ src/main/java/com/kece/fanta/service/impl/DishServiceImpl.java
@@ -1,6 +1,8 @@
 package com.kece.fanta.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.kece.fanta.common.R;
 import com.kece.fanta.dto.DishDto;
 import com.kece.fanta.entity.Dish;
 import com.kece.fanta.entity.DishFlavor;
@@ -8,6 +10,7 @@ import com.kece.fanta.mapper.DishMapper;
 import com.kece.fanta.service.DishFlavorService;
 import com.kece.fanta.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -33,6 +36,42 @@ public class DishServiceImpl extends ServiceImpl<DishMapper, Dish> implements Di
             item.setDishId(dishId);
             return item;
         }).collect(Collectors.toList());
+        dishFlavorService.saveBatch(flavors);
+    }
+
+    @Override
+    public DishDto getByIdWithFlavor(Long id) {
+        // 查询菜品信息
+        Dish dish = this.getById(id);
+
+        //查询菜品口味信息
+        LambdaQueryWrapper<DishFlavor> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.eq(DishFlavor::getDishId,id);
+        List<DishFlavor> dishFlavor = dishFlavorService.list(lambdaQueryWrapper);
+
+
+        DishDto dishDto = new DishDto();
+        BeanUtils.copyProperties(dish,dishDto);
+        dishDto.setFlavors(dishFlavor);
+        return dishDto;
+    }
+
+    @Override
+    public void updateWithFlavor(DishDto dishDto) {
+        this.updateById(dishDto);  //更新菜品信息
+
+        // 删除菜品对应的口味信息
+        LambdaQueryWrapper<DishFlavor> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(DishFlavor::getDishId,dishDto.getId());
+        dishFlavorService.remove(queryWrapper);
+
+        // 获取dishDto中的菜品信息
+        List<DishFlavor> flavors = dishDto.getFlavors();
+        flavors = flavors.stream().map((item)->{
+            item.setDishId(dishDto.getId());
+            return item;
+        }).toList();
+
         dishFlavorService.saveBatch(flavors);
     }
 }
